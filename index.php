2647a6327a65f7809514fa1f15ac6f57ef02e574
Raine Padilla <epadilla7@mail.greenriver.edu>
2018-02-26 20:38:30 -0500

    Finished making form 2 sticky and implemented validation

--- index.php
+++ index.php
@@ -34,15 +34,61 @@ $f3->route('GET|POST /form1', function($f3){
 
        $errorArray = validateFormOne($fName, $lName, $age, $phone, $f3);
 
+       $success = sizeof($errorArray) == 0;
+
+       // Send to hive for templating
         $f3->set('fName', $fName);
         $f3->set('lName', $lName);
         $f3->set('age', $age);
         // Gender not set because it is not sticky
         $f3->set('phone', $phone);
         $f3->set('errors', $errorArray);
+
+        if($success)
+        {
+            // Create member object
+            if(isset($_POST['premium']))
+            {
+                $member = new Premium($fName, $lName, $age, $gender, $phone);
+            }
+            else
+            {
+                $member = new Member($fName, $lName, $age, $gender, $phone);
+            }
+
+            $f3->set('SESSION.member', $member);
+            $f3->reroute('./form2');
+        }
+
     }
     $template = new Template();
     echo $template->render('views/personalinfo.html');
+});
+
+$f3->route('GET|POST /form2', function($f3) {
+    if(isset($_POST['submit']))
+    {
+        // Include validate file
+        include 'model/validate.php';
+
+        $email = $_POST['email'];
+        $state = $_POST['state'];
+        $seeking = $_POST['seeking'];
+        $bio = $_POST['bio'];
+
+        $errorArray = validateFormTwo($email);
+        $success = sizeof($errorArray) == 0;
+
+        // send to hive for templating
+        $f3->set('email', $email);
+        // State not sticky
+        // Seeking not sticky
+        $f3->set('bio', $bio);
+        $f3->set('errors', $errorArray);
+    }
+
+    $template = new Template();
+    echo $template->render('views/profile.html');
 });
 
 $f3->run();
